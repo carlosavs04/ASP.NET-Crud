91f9188c1e0d232fd6d60f2975ee21f95e430870
carlosavs04 <109000628+carlosavs04@users.noreply.github.com>
2024-04-13 19:52:35 -0600

    DbSets

--- AppWebs/Data/ApplicationDbContext.cs
+++ AppWebs/Data/ApplicationDbContext.cs
@@ -11,8 +11,16 @@ namespace AppWebs.Data
         }
 
         public DbSet<Customers> Customers { get; set; }
+        public DbSet<Contacts> Contacts { get; set; }
+        public DbSet<Orders> Orders { get; set; }
         public DbSet<Products> Products { get; set; }
         public DbSet<Product_Categories> Product_Categories { get; set; }
+        public DbSet<Inventories> Inventories { get; set; }
+        public DbSet<Regions> Regions { get; set; }
+        public DbSet<Countries> Countries { get; set; }
+        public DbSet<Locations> Locations { get; set; }
+        public DbSet<Warehouses> Warehouses { get; set; }
+        public DbSet<Employees> Employees { get; set; }
 
         [DbFunction(Schema = "dbo")]
         public static int fn_ProductCategory_count(int pCategoryId)
