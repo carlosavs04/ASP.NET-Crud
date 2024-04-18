0f1ed9d340f9f428641d8e03fa97a5cf55dd1d52
Luis Angel Zapata <dev.luis.zapata@gmail.com>
2024-04-18 00:22:04 -0600

    Locations and Warehouses

---
+++ AppWebs/Controllers/LocationController.cs
@@ -0,0 +1,128 @@
+﻿using AppWebs.Data;
+using AppWebs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppWebs.Controllers
+{
+    public class LocationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LocationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Locations> locations = _context.Locations.Include(l => l.Countries).ToList();
+            return View(locations);
+        }
+
+        public IActionResult Create()
+        {
+            var countries = _context.Countries.ToList();
+            ViewBag.Countries = new SelectList(countries, "CountryId", "CountryName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Locations location)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_context.Locations.Any(l => l.LocationId == location.LocationId))
+                {
+                    ModelState.AddModelError("LocationId", "Location Id already exists");
+                    var countries = _context.Countries.ToList();
+                    ViewBag.Countries = new SelectList(countries,"CountryId", "CountryName");
+                    return View();
+                }
+
+                _context.Locations.Add(location);
+                _context.SaveChanges();
+
+                TempData["Message"] = "Location created successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Details(int? id, bool isEdit = false, bool isDelete = false)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var location = _context.Locations.Include(c => c.Countries).FirstOrDefault(l => l.LocationId == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            ViewBag.IsEdit = isEdit;
+            ViewBag.IsDelete = isDelete;
+
+            return View(location);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var location= _context.Locations.Find(id);
+
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            var countries = _context.Countries.ToList();
+            ViewBag.Countries = new SelectList(countries, "CountryId", "CountryName");
+
+            return View(location);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Locations locations)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Locations.Update(locations);
+                _context.SaveChanges();
+
+                TempData["Message"] = "Location updated successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult DeleteLocation(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var location = _context.Locations.Find(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            _context.Locations.Remove(location);
+            _context.SaveChanges();
+            TempData["Message"] = "Location deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AppWebs/Controllers/WarehouseController.cs
@@ -0,0 +1,123 @@
+﻿using AppWebs.Data;
+using AppWebs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace AppWebs.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WarehouseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Warehouses> warehouses = _context.Warehouses.Include(l => l.Locations).ToList();
+            return View(warehouses);
+        }
+
+        public IActionResult Create()
+        {
+            var locations = _context.Locations.ToList();
+            ViewBag.Locations = new SelectList(locations, "LocationId", "City");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Warehouses warehouses)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Warehouses.Add(warehouses);
+                _context.SaveChanges();
+
+                TempData["Message"] = "Warehouse created successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Details(int? id, bool isEdit = false, bool isDelete = false)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var warehouse = _context.Warehouses.Include(l => l.Locations).FirstOrDefault(w => w.WarehouseId == id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+            ViewBag.IsEdit = isEdit;
+            ViewBag.IsDelete = isDelete;
+
+            return View(warehouse);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var warehouse = _context.Warehouses.Find(id);
+
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+
+            var locations = _context.Locations.ToList();
+            ViewBag.Locations = new SelectList(locations, "LocationId", "City");
+
+            return View(warehouse);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Warehouses warehouse)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Warehouses.Update(warehouse);
+                _context.SaveChanges();
+
+                TempData["Message"] = "Warehouse updated successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult DeleteWarehouse(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var warehouse = _context.Warehouses.Find(id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+
+            _context.Warehouses.Remove(warehouse);
+            _context.SaveChanges();
+            TempData["Message"] = "Location deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
+
