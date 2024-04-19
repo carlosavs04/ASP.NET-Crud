ab3ee8caee6d8bfa55a196b543398978b4b3c3bd
Luis Angel Zapata <dev.luis.zapata@gmail.com>
2024-04-19 16:51:04 -0600

    Inventories read only

---
+++ AppWebs/Controllers/InventoryController.cs
@@ -0,0 +1,50 @@
+﻿using AppWebs.Data;
+using AppWebs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppWebs.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InventoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Inventories> inventories = _context.Inventories.Include(w => w.Warehouses).Include(p => p.Products).ToList();
+            return View(inventories);
+        }
+
+        public IActionResult Create()
+        {
+            var products = _context.Products.ToList();
+            var warehouses = _context.Warehouses.ToList();
+            ViewBag.Products = new SelectList(products, "ProductId", "ProductName");
+            ViewBag.Warehouses = new SelectList(warehouses, "WarehouseId", "WarehouseName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Inventories inventories)
+        {
+            if (ModelState.IsValid)
+            {
+
+                    _context.Inventories.Add(inventories);
+                    _context.SaveChanges();
+                    TempData["Message"] = "Inventory created successfully";
+                    return RedirectToAction("Index");
+                
+            }
+
+            return View();
+        }
+    }
+}
